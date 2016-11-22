d51fdc7af8d0d8cf95ea84a144acaab86df589c1
Niels Boehm <blubberdiblub@gmail.com>
2016-11-22 09:15:48 +0100

    Create compressed versions of files.

--- conf.py
+++ conf.py
@@ -647,9 +647,24 @@ FILTERS = {
 
 # Expert setting! Create a gzipped copy of each generated file. Cheap server-
 # side optimization for very high traffic sites or low memory servers.
-# GZIP_FILES = False
+GZIP_FILES = True
 # File extensions that will be compressed
-# GZIP_EXTENSIONS = ('.txt', '.htm', '.html', '.css', '.js', '.json', '.atom', '.xml')
+GZIP_EXTENSIONS = (
+    '.txt',
+    '.htm',
+    '.html',
+    '.css',
+    '.map',
+    '.js',
+    '.json',
+    '.atom',
+    '.xml',
+    '.xsl',
+    '.rst',
+    '.md',
+    '.svg',
+    '.ttf',
+)
 # Use an external gzip command? None means no.
 # Example: GZIP_COMMAND = "pigz -k {filename}"
 # GZIP_COMMAND = None
